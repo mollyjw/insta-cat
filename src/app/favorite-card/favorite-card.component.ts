877459730b85162dc8dfa0467f808ce046da6ca4
MollyJW <mollyjwhitener@gmail.com>
2021-02-04 21:06:01 -0600

    favorite button on single cat comp works; added some titles; need to add delete favorite function, style, and push to heroku

--- src/app/favorite-card/favorite-card.component.ts
+++ src/app/favorite-card/favorite-card.component.ts
@@ -1,16 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Favorite } from '../models-and-mocks/favorite.model';
+import { FormControl } from '@angular/forms';
+import { TooltipPosition } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-favorite-card',
   templateUrl: './favorite-card.component.html',
-  styleUrls: ['./favorite-card.component.css']
+  styleUrls: ['./favorite-card.component.css'],
+
+  encapsulation: ViewEncapsulation.None,
+
 })
 export class FavoriteCardComponent implements OnInit {
 
   @Input() favorite: Favorite
   favoriteImg: string
   favorites: Favorite[] = []
+  positionOptions: TooltipPosition[] = ['after', 'before', 'above', 'below', 'left', 'right'];
+  position = new FormControl(this.positionOptions[2]);
 
   constructor() { }
 
--- src/app/single-cat/single-cat.component.ts
+++ src/app/single-cat/single-cat.component.ts
@@ -1,25 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { TooltipPosition } from '@angular/material/tooltip';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Cat } from '../models-and-mocks/cat.model';
 import { CatService } from '../services/cat.service';
+import { FavoriteService } from '../services/favorite.service';
 
 @Component({
   selector: 'app-single-cat',
   templateUrl: './single-cat.component.html',
-  styleUrls: ['./single-cat.component.css']
+  styleUrls: ['./single-cat.component.css'],
+
+  encapsulation: ViewEncapsulation.None,
+
 })
 export class SingleCatComponent implements OnInit {
   cat: Cat
   catImg: string
   positionOptions: TooltipPosition[] = ['after', 'before', 'above', 'below', 'left', 'right'];
   position = new FormControl(this.positionOptions[2]);
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+
   private sub = new Subscription()
   constructor(
     private catService: CatService,
-    private route: ActivatedRoute
+    private favoriteService: FavoriteService,
+    private route: ActivatedRoute,
+    private _snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
@@ -45,6 +56,35 @@ export class SingleCatComponent implements OnInit {
         }
       })
     )
+  }
+
+
+  openSnackBar() {
+    this._snackBar.open('Added to your favorites!', 'Close', {
+      duration: 2500,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition
+    });
+  }
+
+  favorite(id: string) {
+    console.log(id)
+    const params = {
+      imageId: id
+    }
+    this.sub.add(
+      this.favoriteService.addToFavorites(params).subscribe(
+        data => {
+          if (data) {    }
+      }, error => {
+        if (error) {
+          console.log(error)
+        }
+      })
+
+    )
+    this.openSnackBar
+    console.log("favorite added")
   }
 
   ngOnDestroy() {
