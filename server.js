25ee93a16cd2729047a62cce388c678bcdf36a35
MollyJW <mollyjwhitener@gmail.com>
2021-02-07 17:29:10 -0600

    fixed typo in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/myapp'));
+app.use(express.static(__dirname + '/dist/insta-cat'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/insta-cat/index.html'));});
